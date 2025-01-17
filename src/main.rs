e5be5f8e0a54b1fe6feb8ca47a45e9ea533a3d72
Max Meindl <max_meindl@gmx.de>
2025-01-17 12:48:57 +0100

    stage 2 code.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ use std::io;
 
 use crossterm::event::{self, KeyCode, KeyEventKind};
 use ratatui::{
+    layout::{Constraint, Layout},
     prelude::{Buffer, Rect},
-    style::Stylize,
+    style::{Color, Style, Stylize},
+    symbols::border,
     text::Line,
-    widgets::Widget,
+    widgets::{Block, Gauge, Widget},
     DefaultTerminal, Frame,
 };
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
 
-    let mut app = App { exit: false };
+    let mut app = App {
+        exit: false,
+        progress_bar_color: Color::Green,
+    };
 
     // App runs on the main thread.
     let app_result = app.run(&mut terminal);
@@ -23,6 +28,7 @@ fn main() -> io::Result<()> {
 
 pub struct App {
     exit: bool,
+    progress_bar_color: Color,
 }
 
 impl App {
@@ -47,14 +53,62 @@ impl App {
     fn handle_key_event(&mut self, key_event: event::KeyEvent) -> io::Result<()> {
         if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
             self.exit = true;
+        } else if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('c') {
+            if self.progress_bar_color == Color::Green {
+                self.progress_bar_color = Color::Yellow;
+            } else {
+                self.progress_bar_color = Color::Green;
+            }
         }
+
         Ok(())
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        // Split the screen vertically in a 20:80 ratio.
+        // Top is used for title, bottom for the progress gauge.
+        let vertical_layout =
+            Layout::vertical([Constraint::Percentage(20), Constraint::Percentage(80)]);
+        let [title_area, gauge_area] = vertical_layout.areas(area);
+
         // Render a title in the top of the layout
-        Line::from("Process overview").bold().render(area, buf);
+        Line::from("Process overview")
+            .bold()
+            .render(title_area, buf);
+
+        // Prepare the widgets for the bottom part of the layout.
+        let instructions = Line::from(vec![
+            " Change color ".into(),
+            "<C>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ])
+        .centered();
+
+        // Block to be displayed around the progress bar.
+        let block = Block::bordered()
+            .title(Line::from(" Background processes "))
+            .title_bottom(instructions)
+            .border_set(border::THICK);
+
+        // Progress bar with label on it.
+        let progress_bar = Gauge::default()
+            .gauge_style(Style::default().fg(self.progress_bar_color))
+            .block(block)
+            .label(format!("Process 1: 50%"))
+            .ratio(0.5);
+
+        // Render the progress bar in the gauge area, with a fixed height of 3 lines (2 for block, 1 for bar)
+        progress_bar.render(
+            Rect {
+                x: gauge_area.left(),
+                y: gauge_area.top(),
+                width: gauge_area.width,
+                height: 3,
+            },
+            buf,
+        );
     }
 }
